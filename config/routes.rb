0e159e7eada2ac9170100e703007393d8181e134
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-07 13:21:09 -0600

    Add associations, sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
+  root 'sessions#home'
+  
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :comments
   resources :users
   resources :posts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20201107182944_create_comments.rb
+++ db/migrate/20201107182944_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.resources :user
-      t.resources :post
+      t.integer :user_id
+      t.integer :post_id
 
       t.timestamps
     end
